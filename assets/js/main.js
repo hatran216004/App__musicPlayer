7833a84ddfac4ad301a44e532bab0d2f34d69c85
hatran216004 <qiqisokiu.321@gmail.com>
2024-01-28 03:08:09 +0700

    HTML CSS

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+/**
+ * 1. Render songs
+ * 2. Scroll top
+ * 3. Play / pause / seek
+ * 4. CD rotate
+ * 5. Next / previous
+ * 6. Random
+ * 7. Next / Repeat when ended
+ * 8. Active song
+ * 9. Scroll active song into view
+ * 10. Play song when click
+ */
